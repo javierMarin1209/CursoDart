08b121196ec6db1b5e8784d4d78a9c8e24a66f80
JavierMarin97 <41657658+JavierMarin97@users.noreply.github.com>
2020-06-03 22:34:50 -0500

    Clase 7

--- bin/Curso1.dart
+++ bin/Curso1.dart
@@ -1,26 +1,32 @@
 import 'dart:ffi';
 
 void main(List<String> arguments) {
-  Map<int,String> map;
-  map ={
-    1:"Rojo",
-    2:"Azul",
-  };
 
-  print(map);
+  //Final vs const
+  //Final se puede instanciar en tiempo de ejecucion y const no
 
-  map[3]="verde";
+  final valor = [1,2,3,4];
+  print(valor); //Imprimimos el array [1,2,3,4], valor asignado inicialmente
+  valor.add(5); //Agregamos un valor al arrray
+  print(valor); //Imprimimos el array y observamos que el valor inicial del array ha cambiado
 
-  print(map);
 
-  map[4]="Rosa";
 
-  print(map);
+  //Const
+  //La asignacion de const a un objeto si hace que los valores sean inmutables
+  const valor2 = [1,2,3,4];
+  print(valor2);
+  valor2.add(5); //Error al intentar mutar el valor inicial del array
+  print(valor2);
 
-  map.remove(2);
 
-  print(map);
+  //Si intentamos que la lista asignada como final no sea mutable:
+  final valor3 = const [1,2,3,4];
 
-  print(map.isEmpty);
-  print(map.length);
+  //Imposible de hacer o asignar
+  //const valor = final [1,2,3,4];
+
+  //Posible de asignar para que el valor sea inmutable
+  //const valor = const [1,2,3,4];
+
 }
