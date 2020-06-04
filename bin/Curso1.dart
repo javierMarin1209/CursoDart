3ac008494254f391d0332ddcb15d2161f64d687f
JavierMarin97 <41657658+JavierMarin97@users.noreply.github.com>
2020-06-03 21:54:39 -0500

    commit inicial

---
+++ bin/Curso1.dart
@@ -0,0 +1,14 @@
+
+import 'dart:ffi';
+
+void main(List<String> arguments) {
+  int varInt = 1;
+  double varDouble = 1.0;
+  String varString= "Hola";
+  bool varBool=true;
+
+  print(varInt);
+  print(varDouble);
+  print(varString);
+  print(varBool);
+}
---
+++ test/Curso1_test.dart
@@ -0,0 +1,8 @@
+import 'package:Curso1/Curso1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
